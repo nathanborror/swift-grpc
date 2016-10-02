9dc93feeebeaf3c44b680e376435978d9ba6a8e5
Nathan Borror <nathan@nathanborror.com>
2016-10-01 18:44:41 -0700

    Removing minor

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SwiftGRPC",
     dependencies: [
-        .Package(url: "https://github.com/apple/swift-protobuf.git", majorVersion: 0, minor: 9),
+        .Package(url: "https://github.com/apple/swift-protobuf.git", majorVersion: 0),
     ]
 )
