42ad57ed918e9d4737a6da0d3d0b4b8b4adf2514
Nathan Borror <nathan@nathanborror.com>
2016-10-06 18:34:36 -0700

    Connecting on write and disconnecting on STREAM_CLOSED flag.

--- Sources/grpc.swift
+++ Sources/grpc.swift
@@ -10,56 +10,69 @@ import Foundation
 import SwiftProtobuf
 
 public class GRPC: NSObject {
-    
+
     public typealias Callback = ([UInt8]) -> Void
-    
+
     let http: Http2
     var callbacks: [StreamID: Callback]
-    
-    public var isConnected = false
-    
+
     public init(url: URL) {
         self.http = Http2(url: url)
         self.callbacks = [:]
         super.init()
         self.http.delegate = self
-        self.http.connect()
     }
-    
+
     public func write(path: String, data: ProtobufMessage, callback: @escaping Callback) {
+        self.http.connect()
+
         let stream = http.streams.next()
         callbacks[stream] = callback
-        
+
         let headers = [
             (":method", "POST"),
             (":scheme", "http"),
             (":path", path),
             ("content-type", "application/grpc+proto"),
             ("te", "trailers"),
-        ]
+            ]
         let frame = Frame(headers: headers, stream: stream, flags: .endHeaders)
         http.write(frame: frame)
-        
+
         let payload = try! data.serializeProtobufBytes()
         var bytes: [UInt8] = [0, 0, 0, 0, UInt8(payload.count)]
         bytes += payload
-        
+
         let dataFrame = Frame(data: bytes, stream: stream, flags: .endStream)
         http.write(frame: dataFrame)
     }
 }
 
 extension GRPC: Http2Delegate {
-    
+
     public func clientConnected(http: Http2) {
-        isConnected = true
     }
-    
+
     public func client(http: Http2, hasFrame frame: Frame) {
+
+        // Ignore frames with a stream ID of 0
+        guard frame.stream > 0 else { return }
+
+        // Process data and send it over callback
         if frame.type == .data {
             guard let payload = frame.payload else { return }
             let bytes = Array(payload[5..<payload.count])
             callbacks[frame.stream]?(bytes)
+        }
+
+        // Remove callback when we receive a stream closed flag
+        if frame.flags == .streamClosed {
+            callbacks.removeValue(forKey: frame.stream)
+        }
+
+        // Disconnect if there are no more callbacks
+        if callbacks.count == 0 {
+            http.disconnect()
         }
     }
 }
