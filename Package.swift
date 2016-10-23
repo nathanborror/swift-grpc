fd824ce7ee9f369597d81766c8e24b747d89535b
Nathan Borror <nathan@nathanborror.com>
2016-10-22 21:33:44 -0700

    Specifying version

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftGRPC",
     dependencies: [
-        .Package(url: "https://github.com/apple/swift-protobuf.git", majorVersion: 0),
-        .Package(url: "https://github.com/nathanborror/swift-http2.git", majorVersion: 0),
+        .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0, 9, 25)),
+        .Package(url: "https://github.com/nathanborror/swift-http2.git", Version(0, 1, 2)),
     ]
 )
